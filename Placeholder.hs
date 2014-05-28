44d2905803d32cc5b1386f6265e60693953365d1
Mathias Bartl <mathiasbartl@web.de>
2014-05-28 20:21:03 +0200

    Trying to do some lookup code

--- Placeholder.hs
+++ Placeholder.hs
@@ -19,12 +19,17 @@
 
 
 module Data.HashTables.IO.Placeholder 
-	( ConcurrentHashTable,  insert, Data.HashTables.IO.Placeholder.lookup, delete, 
+	( ConcurrentHashTable,  insert,  delete, 
 	)
 	where
 
-import GHC.IORef(IORef(IORef))
+import GHC.IORef(IORef(IORef), readIORef)
 import Data.Array.Unboxed(UArray)
+import Data.Hashable(Hashable, hash)
+-- import Data.Array.IArray((!))
+import Data.Array.Unboxed((!))
+import Data.Bits((.&.)) 
+
 
 -- Kempty : empty, K : neverchanging key
 data Key key = Kempty | K key 
@@ -42,28 +47,48 @@ min_size_log = 3
 min_size = 2 ^ min_size_log --must be power of 2, compiler should turn this into a constant
 
 
-type SlotsIndex = Int
+getMask:: Int -> Mask
+getMask size_log = if size_log == 1 then 1 else (getMask ( size_log -1) )* 2 + 1 
 
-type Slots key val = UArray SlotsIndex (State key val)
+type SlotsIndex = Int
+type Mask = SlotsIndex
+
+
+type Slots key val = UArray SlotsIndex (State key val) --TODO issue accessing the array generates a full copy, fix this latter 
 
 data ConcurrentHashTable key val = ConcurrentHashTable {
-		slots :: Slots key val}
+		slots :: Slots key val --TODO, even if imutable it should be an IORef otherwise every handl to the hashmap will contain the whole array
+		, mask :: Mask --TODO make this IORef, in any case since resizing there should be multipe masks
+}
 
 
-
-
-
+-- does not terminate if array is full, and key is not in it
+-- TODO, use fitting hash function
+--getSlot :: Hashable key => Slots key val -> Mask -> key -> State key val
+--getSlot :: Hashable key => UArray SlotsIndex (State key val) -> Mask -> key -> State key val
+getSlot slots mask key = slots ! ( hsh key mask) --TODO apply collision treatment
+		where hsh :: (Hashable key) => key -> Mask -> SlotsIndex
+		      hsh k m = (hash k)  .&. m--TODO apply mask
+ --TODO use forall
 -- new :: IO (ConcurrentHashTable a b)
 -- new = return ConcurrentHashTable
 
 insert :: ConcurrentHashTable key val -> key -> val -> IO ()
 insert hash k v = return ()
-
-lookup :: ConcurrentHashTable key val -> key -> IO ( Maybe val)
-lookup table k = return Nothing
---	where getSlot :: ConcurrentHashTable key val -> key -> IO (State key val)
-
+{--
+lookup :: Hashable key => ConcurrentHashTable key val -> key -> IO ( Maybe val)
+lookup table k = do
+		slot <- getSlot table k
+		slotkey <- readIORef ( key slot)
+		slotvalue <- readIORef (value slot)
+		return $ getValue slotkey slotvalue
+	where getSlot :: ConcurrentHashTable key val -> key -> State key val
+	      getSlot tbl key = getSlot (slots tbl) (mask tbl)  
+ 
+	      getValue :: Key key -> Value val -> Maybe val
+	      getValue Kempty _ = Nothing
+	      getValue (K _) v  = Just v 
+--}
 delete :: ConcurrentHashTable key val -> key -> IO ()
 delete hash k =  return ()
 	
-
