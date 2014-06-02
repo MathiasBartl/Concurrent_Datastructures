c2cc62dafd165af1cc9a15d7f871568eae838c98
Mathias Bartl <mathiasbartl@web.de>
2014-06-02 06:56:49 +0200

    Reprobe

--- Placeholder.hs
+++ Placeholder.hs
@@ -36,6 +36,16 @@ import Data.Atomics
 --todo restrict and qualify
 
 
+
+min_size_log = 3
+min_size = 2 ^ min_size_log --must be power of 2, compiler should turn this into a constant
+
+
+getMask:: Int -> Mask
+getMask size = size -1 --if size_log == 1 then 1 else (getMask ( size_log -1) )* 2 + 1
+
+--data representation
+---------------------------------------------------------------------------------------------------------------------------------
 -- Kempty : empty, K : neverchanging key
 data Key key = Kempty | K key 
 -- T : empty, tombstone, Tp : tombstone primed, V : value, Vp : value primed
@@ -47,13 +57,13 @@ data State k v =   State {
 				}
 
 
+data Kvs k v =   Kvs {
+	newkvs :: IORef ( Maybe (Kvs k v))
+	,slots :: Slots k v 
+}
 
-min_size_log = 3
-min_size = 2 ^ min_size_log --must be power of 2, compiler should turn this into a constant
 
-
-getMask:: Int -> Mask
-getMask size_log = if size_log == 1 then 1 else (getMask ( size_log -1) )* 2 + 1 
+ 
 
 type SlotsIndex = Int
 type Mask = SlotsIndex
@@ -62,10 +72,11 @@ type Mask = SlotsIndex
 type Slots key val = UArray SlotsIndex (State key val) --TODO issue accessing the array generates a full copy, fix this latter 
 
 data ConcurrentHashTable key val = ConcurrentHashTable {
-		slots :: Slots key val --TODO, even if imutable it should be an IORef otherwise every handl to the hashmap will contain the whole array
-		, mask :: Mask --TODO make this IORef, in any case since resizing there should be multipe masks
+		--slots :: Slots key val --TODO, even if imutable it should be an IORef otherwise every handl to the hashmap will contain the whole array		
+		  kvs :: IORef(Kvs key val)
+		--,mask :: Mask --TODO make this IORef, in any case since resizing there should be multipe masks
 }
-
+--------------------------------------------------------------------------------------------------------------------------------------------
 
 -- does not terminate if array is full, and key is not in it
 -- TODO, use fitting hash function
@@ -73,13 +84,39 @@ data ConcurrentHashTable key val = ConcurrentHashTable {
 --getSlot :: Hashable key => UArray SlotsIndex (State key val) -> Mask -> key -> State key val
 --getSlot :: (Data.Array.Unboxed.IArray a e, Hashable key) =>  a SlotsIndex e -> Mask -> key -> e
 --getSlot :: (Data.Array.Unboxed.IArray a Int, Hashable key) =>  a SlotsIndex Int -> Mask -> key -> Int
-getSlot :: (Data.Array.Unboxed.IArray a (State key value), Hashable key) =>  a SlotsIndex (State key value) -> Mask -> key -> (State key value)
-getSlot slots mask key = slots ! ( hsh key mask) --TODO apply collision treatment
+getSlot :: forall key value a. (Data.Array.Unboxed.IArray a (State key value), Hashable key, Eq key) =>  a SlotsIndex (State key value) -> Mask -> key -> IO(State key value)
+getSlot slots mask key =  do	--slot <- (return ( slots ! ( hsh key mask)))::IO(State key value) 
+				--oldkey <- (readKeySlot slot)::IO(Key key)
+				idx <- return $ hsh key mask
+				newkey <- (return $ K key)::IO(Key key)
+				--slot <- (if full oldkey newkey then return slot else return slot)::IO(State key value) --TODO apply collision treatment
+				slot <- getSlt slots newkey idx mask
+--collision treatment has to be done again on a write should the key cas fail
+				return slot
 		where hsh :: (Hashable key) => key -> Mask -> SlotsIndex
 		      hsh k m = (hash k)  .&. m
- --TODO use forall
--- new :: IO (ConcurrentHashTable a b)
--- new = return ConcurrentHashTable
+		      collision :: SlotsIndex -> Mask -> SlotsIndex
+		      collision idx mask = (idx +1) .&. mask
+		      full :: Key key -> Key key -> Bool
+		      full  Kempty _ = False
+		      full  k1 k2 = not (keyComp k1 k2)
+		      --getSlt:: Slots key value -> Key key -> SlotsIndex -> Mask -> IO(State key value)--TODO fix type
+		      getSlt slots newkey idx mask = do
+							slot <- (return (slots !  idx))::IO(State key value)
+							oldkey <- (readKeySlot slot)::IO(Key key)
+							slot <- (if full oldkey newkey then getSlt slots newkey (collision idx mask) mask else return slot)::IO(State key value)
+							return slot 
+
+--new :: IO (ConcurrentHashTable a b)
+--new = return $ ConcurrentHashTable $ newIORef $ Nothing array $ (0 , min_size -1) --TODO
+
+
+
+keyComp:: Eq key => Key key -> Key key -> Bool
+keyComp Kempty Kempty = True
+keyComp Kempty _      = False
+keyComp _     Kempty  = False
+keyComp (K k1) (K k2) = k1 == k2 --TODO eqality on key waht about hashes
 
 insert :: ConcurrentHashTable key val -> key -> val -> IO ()
 insert hash k v = return ()
@@ -114,7 +151,7 @@ readValueSlot state = do
 --TODO collision treatment	
 
 
---TODO Question When are 2 Keys equal, and why does ticket not require a to be in class eq
+--TODO Question When are 2 Keys equal, and why does ticket not require a to be in class eq, how exactly does the COMPARE part work
 casKeySlot :: forall key value. (State key value) -> Key key -> Key key -> IO ( Bool )
 casKeySlot (State ke va) old new = do
 				oldref <- (newIORef old)::IO(IORef (Key key))
@@ -133,3 +170,9 @@ casValueSlot (State ke va) old new = do
 				newticket <- (readForCAS newref) ::IO(Ticket(Value value))
 				(returnvalue, _) <- (casIORef2 va oldticket newticket)::IO(Bool, Ticket(Value value)) 
 				return returnvalue
+
+--TODO, do we need to pass the Hashtable as parameter?
+--TODO assert key is not empty, putval is no empty, but possibly a tombstone
+putIfMatch :: Kvs key value -> Key key -> Value value -> Value value -> IO ()
+putIfMatch kvs key putVal expVal = do   
+					return ()   
